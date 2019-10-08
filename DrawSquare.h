9d32d0ba115e568193922c6190339eb758715794
PerfectObumneme <43893153+PerfectObumneme@users.noreply.github.com>
2019-10-07 20:37:54 -0500

    Update DrawSquare.h

--- DrawSquare.h
+++ DrawSquare.h
@@ -1,13 +1,11 @@
 /*
- Author: Taylor Carloss, Perfect Obumneme, Esteban Orozco,
- Hector Castro, Jake Allen
- Assignment Title: Mario Final Project
- Assignment Desription: This program run the game of Mario
- where the turtle is trying to defeat the Marios.
- Due Date: 12/3/2018
- Date Created: 10/30/2018
- Date Last Modified: 12/3/2018
- */
+Author:  Perfect Obumneme, 
+This program run the game of Mario
+where the turtle is trying to defeat the Marios.
+Due Date: 12/3/2018
+Date Created: 10/30/2018
+Date Last Modified: 12/3/2018
+*/
 
 
 #ifndef DrawSquare_h
