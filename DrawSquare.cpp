fcf41c7d66d1f8d721fddce05a63e66f328c8745
PerfectObumneme <43893153+PerfectObumneme@users.noreply.github.com>
2019-10-07 20:37:21 -0500

    Update DrawSquare.cpp

--- DrawSquare.cpp
+++ DrawSquare.cpp
@@ -1,8 +1,6 @@
 /*
-Author: Taylor Carloss, Perfect Obumneme, Esteban Orozco,
-Hector Castro, Jake Allen
-Assignment Title: Mario Final Project
-Assignment Desription: This program run the game of Mario
+Author:  Perfect Obumneme, 
+This program run the game of Mario
 where the turtle is trying to defeat the Marios.
 Due Date: 12/3/2018
 Date Created: 10/30/2018
